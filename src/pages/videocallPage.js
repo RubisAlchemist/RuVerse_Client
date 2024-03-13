2e1400d609926e52a4076843f4dc6174bef09e55
xognl1124@naver.com <xognl1124@naver.com>
2024-03-13 17:48:12 +0900

    fix remote logic6

--- src/pages/videocallPage.js
+++ src/pages/videocallPage.js
@@ -284,56 +284,6 @@ export default function VideocallPage() {
     ));
   };
 
-  // const getVideoLayout = () => {
-  //   // remote-container가 존재하는지 확인하고, 필요하면 생성합니다.
-  //   let remoteContainer = document.getElementById("remote-container");
-  //   if (!remoteContainer) {
-  //     remoteContainer = document.createElement("div");
-  //     remoteContainer.id = "remote-container";
-  //     document.body.appendChild(remoteContainer); // 이 부분은 적절한 위치에 맞게 조정해야 할 수 있습니다.
-  //   }
-
-  //   // 각 원격 사용자에 대해 비디오 트랙을 재생하는 코드를 추가합니다.
-  //   remoteUsers.forEach((user) => {
-  //     const userContainer = document.getElementById(
-  //       `user-container-${user.uid}`
-  //     );
-  //     if (userContainer && user.videoTrack) {
-  //       user.videoTrack.play(userContainer);
-  //     } else if (!userContainer && remoteContainer) {
-  //       const playerContainer = document.createElement("div");
-  //       playerContainer.id = `user-container-${user.uid}`;
-  //       playerContainer.style.width = "640px";
-  //       playerContainer.style.height = "480px";
-  //       remoteContainer.appendChild(playerContainer);
-  //       user.videoTrack.play(playerContainer);
-  //     }
-  //   });
-
-  //   // 비디오 피드와 버튼을 감싸는 div 생성
-  //   return (
-  //     <div style={{ display: "flex", flexDirection: "row", width: "100%" }}>
-  //       {joinState && localVideoTrack && renderLocalUser()}
-  //       <div
-  //         id="remote-container"
-  //         style={{
-  //           display: "flex",
-  //           justifyContent: "center",
-  //           flexWrap: "wrap",
-  //         }}
-  //       >
-  //         {remoteUsers.map((user) => (
-  //           <div
-  //             key={user.uid}
-  //             id={`user-container-${user.uid}`}
-  //             style={{ margin: "10px" }}
-  //           ></div>
-  //         ))}
-  //       </div>
-  //     </div>
-  //   );
-  // };
-
   // 상단에 정의된 스타일 컴포넌트를 사용
   const getVideoLayout = () => {
     // 로컬 사용자 비디오 렌더링
@@ -350,46 +300,6 @@ export default function VideocallPage() {
       </VideoContainer>
     );
   };
-
-  // // 사용자가 참여하지 않았을 때 표시할 요소들을 렌더링하는 함수
-  // const renderJoinForm = () => {
-  //   return (
-  //     <div
-  //       style={{
-  //         display: "flex",
-  //         flexDirection: "column",
-  //         alignItems: "center",
-  //         justifyContent: "center",
-  //         height: "100vh",
-  //       }}
-  //     >
-  //       <div>
-  //         <StyledInput
-  //           type="text"
-  //           placeholder="주어진 상담소 이름을 입력하세요"
-  //           value={channelName}
-  //           onChange={(e) => setChannelName(e.target.value)}
-  //         />
-  //         <StyledInput
-  //           type="text"
-  //           placeholder="당신의 이름을 입력하세요"
-  //           value={uid}
-  //           onChange={(e) => setUid(e.target.value)}
-  //           style={{ marginRight: 0 }} // Remove the margin for the second input
-  //         />
-  //       </div>
-  //       <StyledImg src={videocallImage} />
-  //       <Button
-  //         onClick={handleJoin}
-  //         variant="contained"
-  //         color="primary"
-  //         style={{ marginTop: "20px" }}
-  //       >
-  //         상담 시작하기
-  //       </Button>
-  //     </div>
-  //   );
-  // };
 
   const renderJoinForm = () => {
     return (
