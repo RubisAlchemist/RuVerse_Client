575968f3e75b12ef6798893f0a1a96ad28f3c312
xognl1124@naver.com <xognl1124@naver.com>
2024-03-19 16:58:51 +0900

    finish final

--- src/pages/videocallPage.jsx
+++ src/pages/videocallPage.jsx
@@ -276,7 +276,7 @@ export default function VideocallPage({
     // if (remoteContainer) {
     //   remoteContainer.innerHTML = ""; // 이를 통해 내부 엘리먼트를 모두 제거
     // }
-    // renderJoinForm();
+    renderJoinForm();
 
     // window.location.reload();
   };
