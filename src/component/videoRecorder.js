0b39d32468ecda9c4433c89b714471be2eaed18d
xognl1124@naver.com <xognl1124@naver.com>
2024-03-20 19:33:43 +0900

    test2

--- src/component/videoRecorder.js
+++ src/component/videoRecorder.js
@@ -11,7 +11,6 @@ const VideoRecorder = forwardRef((props, ref) => {
   const [mediaRecorder, setMediaRecorder] = useState(null);
   const [recordedChunks, setRecordedChunks] = useState([]);
   // const videoRef = useRef(null);
-  const serverAddress = process.env.REACT_APP_BACKEND_ADDRESS_DEV;
 
   useEffect(() => {
     async function getMedia() {
@@ -57,29 +56,6 @@ const VideoRecorder = forwardRef((props, ref) => {
         console.log("MediaRecorder not ready or already recording");
       }
     },
-    // async stopRecording() {
-    //   if (mediaRecorder && mediaRecorder.state === "recording") {
-    //     mediaRecorder.stop();
-    //     setRecording(false);
-    //     console.log("Recording stopped");
-
-    //     // 녹화 중지 후 recordedChunks를 기반으로 Blob 생성 후 반환
-    //     return new Promise((resolve) => {
-    //       const handleDataAvailable = (event) => {
-    //         if (event.data.size > 0) {
-    //           resolve(new Blob(recordedChunks, { type: "video/mp4" }));
-    //         }
-    //       };
-
-    //       mediaRecorder.addEventListener("dataavailable", handleDataAvailable, {
-    //         once: true,
-    //       });
-    //     });
-    //   } else {
-    //     console.log("MediaRecorder not recording");
-    //     return null;
-    //   }
-    // },
     async stopRecording() {
       if (mediaRecorder && mediaRecorder.state === "recording") {
         mediaRecorder.stop();
@@ -88,39 +64,15 @@ const VideoRecorder = forwardRef((props, ref) => {
 
         // 녹화 중지 후 recordedChunks를 기반으로 Blob 생성 후 반환
         return new Promise((resolve) => {
-          mediaRecorder.ondataavailable = async (event) => {
+          const handleDataAvailable = (event) => {
             if (event.data.size > 0) {
-              const blob = new Blob(recordedChunks, { type: "video/mp4" });
-
-              // FormData를 사용하여 서버로 파일 전송
-              const formData = new FormData();
-              formData.append("videoFile", blob, "recording.mp4");
-
-              // 여기에 reduxData 추가
-              if (props.reduxData) {
-                formData.append("reduxData", JSON.stringify(props.reduxData));
-              }
-
-              const uploadURL = `${serverAddress}upload`;
-
-              try {
-                const response = await fetch(uploadURL, {
-                  method: "POST",
-                  body: formData,
-                  // 추가적인 헤더가 필요할 수 있음
-                });
-                const data = await response.json();
-                console.log("Upload successful:", data);
-                resolve(blob); // blob 반환 대신 서버 응답을 반환할 수도 있습니다.
-              } catch (error) {
-                console.error("Upload error:", error);
-                resolve(null);
-              }
+              resolve(new Blob(recordedChunks, { type: "video/mp4" }));
             }
           };
 
-          // 데이터가 사용 가능하게 되었을 때 이벤트를 한 번만 처리하도록 설정
-          mediaRecorder.dispatchEvent(new Event("dataavailable"));
+          mediaRecorder.addEventListener("dataavailable", handleDataAvailable, {
+            once: true,
+          });
         });
       } else {
         console.log("MediaRecorder not recording");
