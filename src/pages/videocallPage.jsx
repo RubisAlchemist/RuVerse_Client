e22e2b8936d4837561cbac87c4a7d597e4ab7f04
jinotter3 <jinotter3@snu.ac.kr>
2024-04-01 02:32:11 +0900

    fix accelgyro & upload logic(fixing overlap in gps, eyetracking)

--- src/pages/videocallPage.jsx
+++ src/pages/videocallPage.jsx
@@ -318,9 +318,9 @@ export default function VideocallPage({
     //   remoteContainer.innerHTML = ""; // 이를 통해 내부 엘리먼트를 모두 제거
     // }
     // renderJoinForm();
-    setTimeout(() => {
-      window.location.reload();
-    }, 3000);
+    // setTimeout(() => {
+    //   window.location.reload();
+    // }, 3000); 
   };
 
   const renderLocalUser = () => {
