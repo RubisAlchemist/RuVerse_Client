a673ba750aae51b4ce31313dab257c2386fed0f6
xognl1124@naver.com <xognl1124@naver.com>
2024-04-02 12:45:39 +0900

    fix logic

--- src/pages/videocallPage.jsx
+++ src/pages/videocallPage.jsx
@@ -349,7 +349,7 @@ export default function VideocallPage({
     // }
     // renderJoinForm();
     // setTimeout(() => {
-    // window.location.reload();
+    window.location.reload();
     // }, 3000);
   };
 
