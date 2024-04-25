5d2aa36113241f7508090c191d600151ed944bbb
xognl1124@naver.com <xognl1124@naver.com>
2024-04-25 13:58:41 +0900

    change joinform css

--- src/component/form/JoinForm.jsx
+++ src/component/form/JoinForm.jsx
@@ -1,7 +1,16 @@
-import { Box, Button, Card, CardMedia, Stack, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardMedia,
+  Stack,
+  TextField,
+  Container,
+} from "@mui/material";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import useCurrentLocation from "../../hooks/useCurrentLocation";
+import "./joinForm.css";
 
 import VideoCallImage from "../../images/videocallImage.png";
 import {
@@ -28,61 +37,65 @@ function JoinForm() {
   };
 
   return (
-    <Stack spacing={2}>
-      <Card>
-        <CardMedia component="img" image={VideoCallImage} />
-      </Card>
+    <Container maxWidth="md" className="responsive-container">
+      <Stack spacing={2} alignItems="center">
+        <Card>
+          <CardMedia component="img" image={VideoCallImage} />
+        </Card>
 
-      <TextField
-        required
-        error={isChannelNameError}
-        label="상담소명"
-        value={channelName}
-        onChange={(e) =>
-          dispatch(
-            onChangeChannelName({
-              value: e.target.value,
-              valid: e.target.validity.valid,
-            })
-          )
-        }
-        helperText={
-          isChannelNameError ? "상담소명은 영문, 숫자만 가능합니다." : ""
-        }
-        inputProps={{
-          pattern: "[a-zA-Z0-9]+",
-        }}
-      />
-      <TextField
-        required
-        error={isUidError}
-        label="유저 이름"
-        value={uid}
-        helperText={isUidError ? "유저 이름은 영문, 숫자만 가능합니다." : ""}
-        onChange={(e) => {
-          console.log(e.target.validity);
-          dispatch(
-            onChangeUid({
-              value: e.target.value,
-              valid: e.target.validity.valid,
-            })
-          );
-        }}
-        inputProps={{
-          pattern: "[a-zA-Z0-9]+",
-        }}
-      />
-      <Box display="flex" justifyContent="flex-end">
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleJoin}
-          disabled={isUidError || isChannelNameError}
-        >
-          상담 시작하기
-        </Button>
-      </Box>
-    </Stack>
+        <TextField
+          required
+          error={isChannelNameError}
+          label="상담소명"
+          value={channelName}
+          onChange={(e) =>
+            dispatch(
+              onChangeChannelName({
+                value: e.target.value,
+                valid: e.target.validity.valid,
+              })
+            )
+          }
+          helperText={
+            isChannelNameError ? "상담소명은 영문, 숫자만 가능합니다." : ""
+          }
+          inputProps={{
+            pattern: "[a-zA-Z0-9]+",
+          }}
+          sx={{ width: "60%" }}
+        />
+        <TextField
+          required
+          error={isUidError}
+          label="유저 이름"
+          value={uid}
+          helperText={isUidError ? "유저 이름은 영문, 숫자만 가능합니다." : ""}
+          onChange={(e) => {
+            console.log(e.target.validity);
+            dispatch(
+              onChangeUid({
+                value: e.target.value,
+                valid: e.target.validity.valid,
+              })
+            );
+          }}
+          inputProps={{
+            pattern: "[a-zA-Z0-9]+",
+          }}
+          sx={{ width: "60%" }}
+        />
+        <Box display="flex" justifyContent="flex-end">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleJoin}
+            disabled={isUidError || isChannelNameError}
+          >
+            상담 시작하기
+          </Button>
+        </Box>
+      </Stack>
+    </Container>
   );
 }
 
