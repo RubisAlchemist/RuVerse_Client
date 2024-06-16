f9f3ac3cd9f8ee0404d939d6719ffa0bdabbcbcc
yongun2 <djkye0001@naver.com>
2024-06-16 18:17:52 +0900

    border 제거

--- src/pages/ChannelEntryPage.jsx
+++ src/pages/ChannelEntryPage.jsx
@@ -23,7 +23,6 @@ const ChannelEntryPage = () => {
       sx={{
         width: "100%",
         height: "100vh",
-        border: "1px solid blue",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
